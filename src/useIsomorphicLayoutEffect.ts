395fb3c7b51c996115ac247336897f646bc901ff
Krishan Patel <krishan711@gmail.com>
2020-12-16 11:30:39 +0000

    .

---
+++ src/useIsomorphicLayoutEffect.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import { getIsRunningOnBrowser } from './browserUtil';
+
+export const useIsomorphicLayoutEffect = getIsRunningOnBrowser() ? React.useLayoutEffect : React.useEffect;
