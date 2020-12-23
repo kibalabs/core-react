04bcb0e71562051ec09efca8d471689bbed5200e
Krishan Patel <krishan711@gmail.com>
2020-12-23 21:07:13 +0000

    Feature - update routing to support SSR + update dependencies (#2)
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .

--- src/routing.tsx
+++ src/routing.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router as ReachRouter, LocationProvider as ReachLocationProvider, useParams as useReachParams, Redirect as ReachRedirect, History as ReachHistory, Link as ReachLink, createHistory as createReachHistory } from '@reach/router'
+import { Router as ReachRouter, LocationProvider as ReachLocationProvider, useParams as useReachParams, Redirect as ReachRedirect, History as ReachHistory, Link as ReachLink, createHistory as createReachHistory, createMemorySource as createReachMemorySource} from '@reach/router'
 
 import { ErrorBoundary } from './errorBoundary';
 import { IMultiChildProps } from './parentComponentProps';
@@ -40,24 +40,31 @@ export const useRouterAuthManager = (): IRouterAuthManager => {
   return authManager;
 }
 
-export interface IRouteProps<PagePropsType = {}> {
+export interface IRouteProps<PagePropsType = Record<string, any>> {
   path?: string;
   default?: boolean;
   uri?: string;
   redirectIfAuth?: string;
   redirectIfNoAuth?: string;
-  page: React.ComponentType<PagePropsType>;
+  page?: React.ComponentType<PagePropsType>;
+  pageElement?: React.ComponentClass<PagePropsType>;
 }
 
 export const Route = (props: IRouteProps): React.ReactElement => {
   const params = useReachParams();
   const authManager = useRouterAuthManager();
+  if (!props.page && !props.pageElement) {
+    throw new Error('One of {page, pageElement} must be passed into each Route');
+  }
+  if (props.page && props.pageElement) {
+    throw new Error('Only ONE of {page, pageElement} must be passed into each Route');
+  }
   if (props.redirectIfNoAuth) {
     if (!authManager) {
       throw new Error('Cannot use redirectIfNoAuth since an authManager has not ben provided to the router');
     }
     if (!authManager.getIsUserLoggedIn()) {
-      // TODO(krish): using history.navigate would be prefereable here but it didnt work, figure out why
+      // TODO(krish): using history.navigate would be preferable here but it didn't work, figure out why
       return <ReachRedirect noThrow to={props.redirectIfNoAuth} />;
     }
   }
@@ -66,13 +73,14 @@ export const Route = (props: IRouteProps): React.ReactElement => {
       throw new Error('Cannot use redirectIfAuth since an authManager has not ben provided to the router');
     }
     if (authManager.getIsUserLoggedIn()) {
-      // TODO(krish): using history.navigate would be prefereable here but it didnt work, figure out why
+      // TODO(krish): using history.navigate would be preferable here but it didn't work, figure out why
       return <ReachRedirect noThrow to={props.redirectIfAuth} />;
     }
   }
   return (
     <ErrorBoundary>
-      <props.page {...params} />
+      {props.page && <props.page {...params} />}
+      {props.pageElement && React.cloneElement(props.pageElement, params)}
     </ErrorBoundary>
   );
 }
@@ -104,4 +112,8 @@ export const Link = (props: ILinkProps): React.ReactElement => {
   return (
     <ReachLink to={props.target}>{props.text}</ReachLink>
   )
+}
+
+export const createStaticHistory = (path: string) => {
+  return createReachHistory(createReachMemorySource(path));
 }
