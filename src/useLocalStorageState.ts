3010f9c0f5811aa2459506e717caa2a8297285a4
Krishan Patel <krishan711@gmail.com>
2020-12-31 15:32:44 +0000

    .

--- src/useLocalStorageState.ts
+++ src/useLocalStorageState.ts
@@ -52,11 +52,6 @@ export default class LocalStorageClient {
 }
 
 export const useLocalStorageState = (name: string, overrideInitialValue?: string | null): [string | null, (newValue: string | null) => void] => {
-  // if (typeof window === 'undefined') {
-  //   console.warn('Cannot use useLocalStorageState without a window present!');
-  //   return [null, (): void => undefined];
-  // }
-
   const localStorage = new LocalStorageClient(window.localStorage);
   const [value, setValue] = React.useState<string | null>((): string | null => {
     if (overrideInitialValue !== undefined) {
