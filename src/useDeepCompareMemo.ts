54877e4d8c71b8f8610a0a387b81624b2906a3f6
Krishan Patel <krishan711@gmail.com>
2021-09-25 10:16:20 +0100

    .

--- src/useDeepCompareMemo.ts
+++ src/useDeepCompareMemo.ts
@@ -3,5 +3,7 @@ import React from 'react';
 import { useDeepCompareRef } from './useDeepCompareRef';
 
 export const useDeepCompareMemo = <T>(callback: () => T, dependencies: unknown[]): T => {
+  // NOTE(krishan711): we purposefully don't include the callback itself as a dependency
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   return React.useMemo(callback, useDeepCompareRef(dependencies));
 };
