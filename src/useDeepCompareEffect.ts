f410cefc391dfba92b2b8fd771a3c9514cdfb7e6
Krishan Patel <krishan711@gmail.com>
2021-11-12 09:21:36 +0000

    .

--- src/useDeepCompareEffect.ts
+++ src/useDeepCompareEffect.ts
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { useDeepCompareRef } from './useDeepCompareRef';
 
-export const useDeepCompareEffect = (callback: () => void, dependencies: unknown[]): void => {
+export const useDeepCompareEffect = (callback: () => void | (() => void), dependencies: unknown[]): void => {
   // NOTE(krishan711): we purposefully don't include the callback itself as a dependency
   // eslint-disable-next-line react-hooks/exhaustive-deps
   React.useEffect(callback, useDeepCompareRef(dependencies));
