d5faae69df82abbb7b57bad757c1ed64ad8f577b
Krishan Patel <krishan711@gmail.com>
2025-01-23 16:52:02 +0000

    .

--- src/routing.tsx
+++ src/routing.tsx
@@ -188,7 +188,16 @@ export function Router<IGlobals, >(props: IRouterProps<IGlobals>): React.ReactEl
       {internals}
     </StaticRouter>
   ) : (
-    <BrowserRouter basename={props.basePath}>
+    <BrowserRouter
+      basename={props.basePath}
+      future={{
+        v7_relativeSplatPath: true,
+        v7_startTransition: true,
+        v7_fetcherPersist: true,
+        v7_normalizeFormMethod: true,
+        v7_partialHydration: true,
+        v7_skipActionErrorRevalidation: true,
+      }}>
       {internals}
     </BrowserRouter>
   );
